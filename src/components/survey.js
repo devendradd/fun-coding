293f84b4cba7b0ffe54f4271799ae1e7333f530b
Devendra Deo <this.devendra@gmail.com>
2018-12-18 23:26:07 +0530

    added react strap with some code changes and bug fixes

--- src/components/survey.js
+++ src/components/survey.js
@@ -1,21 +1,105 @@
 import React, {Component} from 'react';
 
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col, CardGroup } from 'reactstrap';
+
+import './surveyList.css';
+
+
+
 export default class Survey extends Component{
-    constructor(){
-        super();
-        // console.log("data in servey component: ", this.props.dataToChild);
-        console.log("this is constructor of survey................");
+
+    constructor(props){
+
+        super(props);
+
+
+
     }
 
+
+
     render(){
+
+
+
+      var custom_card = {
+
+            float: 'left',
+
+            // height: '400px',
+
+            width: '265px',
+
+            margin : '10px',
+
+            backgroundColor: '#204FA7',
+
+            borderColor: '#204FA7',
+
+            color: 'white',
+
+            overflow:'scroll',
+
+
+
+        }
+
+
+
         return(
-            <div>
-                id: {this.props.id}
-                title: {this.props.title}
-                date: {this.props.date}
-                short description: {this.props.shortDesc}
-                description: {this.props.description}
-            </div>
+
+
+
+           <Card body inverse style={custom_card} id="wrapper" className="jumbotron" >
+
+            <Row>
+
+                <Col xs="12" style={{height: '240px'}}>
+
+                    <CardBody>
+
+                        <CardTitle>Name : Survey</CardTitle>
+
+                        <CardSubtitle>
+
+                            Title:  {this.props.data.title}
+
+                        </CardSubtitle>
+
+                        <CardText>
+
+                            Id: {this.props.data.id} <br/> <br/>
+
+                            Title: {this.props.data.title}<br/> <br/>
+
+                            Date: {this.props.data.date}<br/> <br/>
+
+                            Short Desc: {this.props.data.shortDesc} <br/> <br/>
+
+                            Desc: {this.props.data.description}
+
+                        </CardText>
+
+                    </CardBody>
+
+                </Col>
+
+            </Row>
+
+            {/* <Row>
+
+                <Col xs="12" style={{height: '100px'}}>
+
+                    <Button color="primary" size="sm" block>Like</Button>{' '}
+
+                </Col>
+
+            </Row> */}
+
+        </Card>
+
         );
+
     }
+
 }
--- src/components/surveyList.js
+++ src/components/surveyList.js
@@ -1,52 +1,126 @@
 import React, { Component } from 'react';
+
+import {Table, CardDeck} from 'reactstrap';
+
 import axios from 'axios';
+
 import './surveyList.css';
+
 import Survey from './survey';
 
+
+
 export default class SurveyList extends Component{
-    
+
+
+
     constructor(){
+
         super();
+
         this.state= {data:[], resultItems:''}
+
     }
 
+
+
     componentDidMount(){
-        var dataList=undefined;
+
+
+
         axios.get('http://www.mocky.io/v2/56fcf6871000000e13aed252')
+
             .then(response => {
+
                 console.log("data is :", response.data)
+
                 this.setState({data: response.data});
+
             })
+
             .catch(function (error) {
+
                 console.log(error);
+
             });
+
             // this.setState({data: dataList});
+
     }
+
+
+
+
+
 
 
 
 
     render(){
-        console.log('data for mapping : ',this.state.data);
+
+        // console.log('data for mapping : ',this.state.data);
+
         var resultItems = this.state.data.map((result)=> {
+
+
+
+          // var resultKeys = Object.keys(result);
+
+          // var item = {
+
+          //       id: result[resultKeys[0]],
+
+          //       title: result[resultKeys[1]],
+
+          //       date: result[resultKeys[2]],
+
+          //       shortDesc: result[resultKeys[3]],
+
+          //       description : result[resultKeys[4]]
+
+          // }
+
+
+
             var item = {
+
                 id: result.id,
+
                 title: result.title,
-                data: result.data,
-                shortDesc: result.shortDesc,
+
+                date: result.creation_date,
+
+                shortDesc: result["short_ description"],
+
                 description : result.description
+
             };
 
-            return <Survey element={item} />
+            return <Survey key={item.id} data={item}/>;
+
+
+
         })
 
-        this.setState({resultItems: resultItems});
-        
-        
+
+
+
+
         return(
+
             <div className="suerveyListCOntainer">
-                {this.state.resultItems}
+
+              <CardDeck>
+
+                {resultItems}>
+
+              </CardDeck>
+
             </div>
+
         )
+
     }
+
 }
+
